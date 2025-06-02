73b9e7a78fa24811172ed151e1a722585278845a
Varunyadavgithub <yadav.varun056038@gmail.com>
2025-06-02 16:36:40 +0530

    layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects ";
+import Skills from "./pages/Skills";
+import Experience from "./pages/Experience";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <div className="text-4xl text-red-600">
-      <h1>Vikash kumar</h1>
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <Navbar />
+      </header>
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
